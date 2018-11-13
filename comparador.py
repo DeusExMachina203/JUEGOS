ca69c86084d146b61808104de7f6702e568b3366
jgallarday <44297397+jgallarday@users.noreply.github.com>
2018-11-12 23:41:08 -0500

    Update comparador.py

--- comparador.py
+++ comparador.py
@@ -1,1 +1,32 @@
-
+x=0
+for y in range(matriz):
+    if x==0 and y==0:
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x+1]=="B":
+           matriz[y][x]+=1
+	elif x==ancho-1 and y==alto-1:
+		if matriz[y-1][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y-1][x]=="B":
+			matriz[y][x]+=1
+		if matriz [y][x-1]=="B":
+			matriz[y][x]+=1
+	elif x==0 and y==alto-1:
+		if matriz[y-1][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y-1][x+1]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x+1]=="B":
+			matriz[y][x]+=1
+	elif x==ancho and y==0:
+		if matriz[y][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x]=="B":
+			matriz[y][x]+=1
+		
+    x+=1
