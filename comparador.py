c4331a3ce3626ce7b867cf69e4aca4e34a1a286d
DeusExMachina203 <44448300+DeusExMachina203@users.noreply.github.com>
2018-11-13 00:09:51 -0500

    Update comparador.py

--- comparador.py
+++ comparador.py
@@ -1,92 +1,95 @@
 x=0
-for y in range(matriz):
+for y in range(len(matriz)):
     if x==0 and y==0:
         if matriz[y][x+1]=="B":
             matriz[y][x]+=1
         if matriz[y+1][x]=="B":
             matriz[y][x]+=1
         if matriz[y+1][x+1]=="B":
-           matriz[y][x]+=1
-	elif x==ancho-1 and y==alto-1:
-		if matriz[y-1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz [y][x-1]=="B":
-			matriz[y][x]+=1
-	elif x==0 and y==alto-1:
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x+1]=="B":
-			matriz[y][x]+=1
-	elif x==ancho and y==0:
-		if matriz[y][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x]=="B":
-			matriz[y][x]+=1
-	elif x==0:
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x+1]=="B":
-			matriz[y][x]+=1
-	elif x==ancho-1:
-		if matriz[y-1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x]=="B":
-			matriz[y][x]+=1
-	elif y==0:
-		if matriz[y][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x+1]=="B":
-			matriz[y][x]+=1
-	elif y==alto-1:
-		if matriz[y-1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x+1]=="B":
-			matriz[y][x]+=1
-	else:
-		if matriz[y-1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y-1][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y][x+1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x-1]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x]=="B":
-			matriz[y][x]+=1
-		if matriz[y+1][x+1]=="B":
-			matriz[y][x]+=1
+            matriz[y][x]+=1
+    elif x==ancho-1 and y==alto-1:
+        if matriz[y-1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz [y][x-1]=="B":
+            matriz[y][x]+=1
+    elif x==0 and y==alto-1:
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+    elif x==ancho and y==0:
+        if matriz[y][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+    elif x==0:
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x+1]=="B":
+            matriz[y][x]+=1
+    elif x==ancho-1:
+        if matriz[y-1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+    elif y==0:
+        if matriz[y][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x+1]=="B":
+            matriz[y][x]+=1
+    elif y==alto-1:
+        if matriz[y-1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+    else:
+        if matriz[y-1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y-1][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y][x+1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x-1]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x]=="B":
+            matriz[y][x]+=1
+        if matriz[y+1][x+1]=="B":
+            matriz[y][x]+=1
     x+=1
+print(' ')
+for i in matriz:
+    print(*i, sep=' ')
