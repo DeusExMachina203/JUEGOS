9ebf143d682ae18f829b03617fe9a3b9dfa64bd7
jgallarday <44297397+jgallarday@users.noreply.github.com>
2018-11-12 23:53:10 -0500

    Update comparador.py

--- comparador.py
+++ comparador.py
@@ -28,5 +28,48 @@ for y in range(matriz):
 			matriz[y][x]+=1
 		if matriz[y+1][x]=="B":
 			matriz[y][x]+=1
-		
+	elif x==0:
+		if matriz[y-1][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y-1][x+1]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x+1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x+1]=="B":
+			matriz[y][x]+=1
+	elif x==ancho-1:
+		if matriz[y-1][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y-1][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x]=="B":
+			matriz[y][x]+=1
+	elif y==0:
+		if matriz[y][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x+1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x-1]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y+1][x+1]=="B":
+			matriz[y][x]+=1
+	elif y==alto-1:
+		if matriz[y][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x]=="B":
+			matriz[y][x]+=1
+		if matriz[y][x]=="B":
+			matriz[y][x]+=1
     x+=1
