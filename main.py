13ca049ed92e1096e932cf23a3b2df173a5831bb
DeusExMachina203 <44448300+DeusExMachina203@users.noreply.github.com>
2018-11-13 00:30:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,99 @@ else:
 print(' ')
 for x in pene:
     print(*x, sep=' ')
+for y in range(len(matriz)):
+    for x in range(ancho):
+        if matriz[y][x]=="B":
+            pass
+        elif x==0 and y==0:
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x+1]=="B":
+                matriz[y][x]+=1
+        elif x==ancho-1 and y==alto-1:
+            if matriz[y-1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz [y][x-1]=="B":
+                matriz[y][x]+=1
+        elif x==0 and y==alto-1:
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+        elif x==ancho and y==0:
+            if matriz[y][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+        elif x==0:
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x+1]=="B":
+                matriz[y][x]+=1
+        elif x==ancho-1:
+            if matriz[y-1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+        elif y==0:
+            if matriz[y][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x+1]=="B":
+                matriz[y][x]+=1
+        elif y==alto-1:
+            if matriz[y-1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+        else:
+            if matriz[y-1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y-1][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y][x+1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x-1]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x]=="B":
+                matriz[y][x]+=1
+            if matriz[y+1][x+1]=="B":
+                matriz[y][x]+=1
+print(' ')
+for i in matriz:
+    print(*i, sep=' ')
